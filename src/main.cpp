86ad8ab751af320b6f004aace7abd293c0f69fe8
AmirAdal <amir.adal@cobold.in>
2022-10-16 21:56:11 +0300

    Print ast tree in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <astparser.h>
 #include <filereader.h>
 
+#include <astdebug.h>
+
 void parse_tokenizer(Tokenizer &tokenizer)
 {
     using namespace ast;
@@ -11,54 +13,7 @@ void parse_tokenizer(Tokenizer &tokenizer)
 
     nodes::StatementsNode *result = parser.parse();
 
-    std::cout << "Result: " << result->statements()->size() << std::endl;
-}
-
-void print_node(ast::nodes::Node *node)
-{
-    using namespace ast::nodes;
-
-    if (node->type() == NodeType::BINARY)
-    {
-        BinaryNode *binary = (BinaryNode *)node;
-
-        std::cout << "BinaryNode: " << binary->op() << std::endl;
-
-        print_node(binary->left());
-        print_node(binary->right());
-    }
-    else if (node->type() == NodeType::LITERAL)
-    {
-        LiteralNode *literal = (LiteralNode *)node;
-
-        std::cout << "LiteralNode: " << literal->literal_type() << std::endl;
-
-        if (literal->literal_type() == LiteralType::INT)
-        {
-            std::cout << "Value: " << *(int *)literal->value() << std::endl;
-        }
-
-        if (literal->literal_type() == LiteralType::FLOAT)
-        {
-            std::cout << "Value: " << *(float *)literal->value() << std::endl;
-        }
-
-        if (literal->literal_type() == LiteralType::STRING)
-        {
-            std::cout << "Value: " << (char *)literal->value() << std::endl;
-        }
-
-        if (literal->literal_type() == LiteralType::BOOL)
-        {
-            std::cout << "Value: " << *(bool *)literal->value() << std::endl;
-        }
-
-        if (literal->literal_type() == LiteralType::UNDEFINED)
-        {
-            std::cout << "Value: "
-                      << "UNDEFINED" << std::endl;
-        }
-    }
+    print_nodes(result);
 }
 
 void parse_statements_node(ast::nodes::StatementsNode *statements)
