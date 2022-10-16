05f056a9a5ab66fe7370450cbc4c698924ddc7d4
AmirAdal <amir.adal@cobold.in>
2022-10-16 14:52:26 +0300

    Provided implmentation for filereader.h

--- src/filereader.cpp
+++ src/filereader.cpp
@@ -1,1 +1,31 @@
 #include <filereader.h>
+
+FileReader::FileReader(const char *filename)
+{
+    m_file.open(filename);
+
+    if (!m_file.is_open())
+    {
+        throw std::runtime_error("Could not open file");
+    }
+}
+
+FileReader::~FileReader()
+{
+    m_file.close();
+}
+
+std::string FileReader::read()
+{
+    // Read the whole file into a string
+    std::string content;
+
+    while (!m_file.eof())
+    {
+        std::string line;
+        std::getline(m_file, line);
+        content += line + "\n";
+    }
+
+    return content;
+};
