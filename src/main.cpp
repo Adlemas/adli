a9c1e347dad10064c383c6d549e7ccc2f89fec04
AmirAdal <amir.adal@cobold.in>
2022-10-16 16:23:10 +0300

    Created functions to print tokens and parse tokens

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,29 @@
 #include <astparser.h>
 #include <filereader.h>
 
+void parse_tokenizer(Tokenizer &tokenizer)
+{
+    Parser parser(&tokenizer);
+
+    std::vector<int> result = parser.parse();
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        std::cout << "Result #" << i + 1 << ": " << result[i] << std::endl;
+    }
+}
+
+void print_tokens(Tokenizer &tokenizer)
+{
+    Token *token = tokenizer.next_token();
+
+    while (token->type != Token::_EOF)
+    {
+        std::cout << "Token: " << token->value << std::endl;
+        token = tokenizer.next_token();
+    }
+}
+
 int main()
 {
     // std::string source;
@@ -19,22 +42,12 @@ int main()
     tokenizer.tokenize();
 
     // print tokens
-    // Token *token = tokenizer.next_token();
+    print_tokens(tokenizer);
 
-    // while (token->type != Token::_EOF)
-    // {
-    //     std::cout << "Token: " << '"' << token->value << '"' << std::endl;
-    //     token = tokenizer.next_token();
-    // }
+    tokenizer.reset();
 
-    Parser parser(&tokenizer);
-
-    std::vector<int> result = parser.parse();
-
-    for (int i = 0; i < result.size(); i++)
-    {
-        std::cout << "Result #" << i + 1 << ": " << result[i] << std::endl;
-    }
+    // parse tokens
+    parse_tokenizer(tokenizer);
 
     return 0;
 }
