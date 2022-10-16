83b1373ff5e1df18b97b40a88414df463b9bf93c
AmirAdal <amir.adal@cobold.in>
2022-10-16 13:56:15 +0300

    Provided FileReader class

---
+++ include/filereader.h
@@ -0,0 +1,18 @@
+#ifndef FILE_READER_H
+#define FILE_READER_H
+
+#include <fstream>
+#include <string>
+
+class FileReader
+{
+    std::ifstream m_file;
+
+public:
+    FileReader(const char *filename);
+    ~FileReader();
+
+    std::string read();
+};
+
+#endif
