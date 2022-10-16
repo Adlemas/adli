356b4643219068c6eba73f4fa3636bb67faf62ff
AmirAdal <amir.adal@cobold.in>
2022-10-16 13:56:27 +0300

    Testing astparser

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <tokenizer.h>
+#include <astparser.h>
 
 int main()
 {
@@ -11,13 +12,19 @@ int main()
     tokenizer.tokenize();
 
     // print tokens
-    Token *token = tokenizer.next_token();
+    // Token *token = tokenizer.next_token();
 
-    while (token->type != Token::_EOF)
-    {
-        std::cout << "Token: " << '"' << token->value << '"' << std::endl;
-        token = tokenizer.next_token();
-    }
+    // while (token->type != Token::_EOF)
+    // {
+    //     std::cout << "Token: " << '"' << token->value << '"' << std::endl;
+    //     token = tokenizer.next_token();
+    // }
+
+    Parser parser(&tokenizer);
+
+    int result = parser.parse();
+
+    std::cout << "Result: " << result << std::endl;
 
     return 0;
 }
